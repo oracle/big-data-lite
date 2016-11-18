b24af7e1c148e668e354347910d10e31d140e095
martygubar <martin.gubar@oracle.com>
2016-11-18 17:31:49 -0500

    post_script.sh changed to update bdsg init.d script.

--- scripts/post_script.sh
+++ scripts/post_script.sh
@@ -1,1 +1,4 @@
-echo post_script done
+echo "Updating bdsg startup script in /etc/init.d"
+sudo cp /home/oracle/src/scripts/bdsg /etc/init.d/
+sudo chmod 755 /etc/init.d/bdsg
+sudo chown root:root /etc/init.d/bdsg
