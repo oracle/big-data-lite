923eb8ef541a6a6293b0a074ac0019417d851217
martygubar <martin.gubar@oracle.com>
2017-02-24 12:33:44 -0500

    Fix to Renviron.site

--- scripts/post_script.sh
+++ scripts/post_script.sh
@@ -1,1 +1,7 @@
-echo post_script done
+echo Running post-script
+echo Updating /usr/lib64/R/etc/Renviron.site
+sudo cp /usr/lib64/R/etc/Renviron.site /usr/lib64/R/etc/Renviron.site.sav
+sudo cp ~oracle/src/scripts/Renviron.site /usr/lib64/R/etc/
+sudo chmod 644 /usr/lib64/R/etc/Renviron.site
+sudo chown root:root /usr/lib64/R/etc/Renviron.site
+echo Update complete.
