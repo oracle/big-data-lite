9515882fda2a1f13fe176890832ea1ae12506a00
martygubar <martin.gubar@oracle.com>
2017-05-19 13:57:32 -0400

    update to 1_startmovieap.sh

--- movie/moviedemo/scripts/1_start_movieapp.sh
+++ movie/moviedemo/scripts/1_start_movieapp.sh
@@ -1,5 +1,5 @@
 echo Starting MoviePlex application using KVStore latest state
-cd /home/oracle/movie/moviedemo/nosqldb/scripts
-./startDemoKeepKVState.sh
+sudo service nosql start
+sudo service weblogic-movie start
 echo Go to Firefox and wait for application to start.
-echo http://127.0.0.1:7001/bigdatademo-UI-context-root/index.jsp
+echo http://localhost:7001/movieplex/index.jsp
